54f017b793e0197136992167bccf9f84684eed35
eideun <yed622j@gmail.com>
2023-07-07 17:01:03 +0900

    카트 수정중

--- src/views/cart/cart.js
+++
@@ -1,7 +0,0 @@
-function checkbox() {
-    let check_all = document.querySelector(".select-all");
-    check_all.addEventListener("click", function(e) {
-        e.preventDefault();
-        check_all.style.backgroundColor = "yellow";
-    });
-}
