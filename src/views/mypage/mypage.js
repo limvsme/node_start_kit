daadf0fb140e3b4a56bfe4440621f8eb74b47d94
bluewhite0302 <bluewhite0302@gmail.com>
2023-07-07 16:34:30 +0900

    FEAT: 마이페이지에서 회원정보수정, 주문조회 화면으로 이동할 수 있게 기능 구현

---
+++ src/views/mypage/mypage.js
@@ -0,0 +1,18 @@
+const accountSecurity = document.querySelector("#account-security");
+const accountOrders = document.querySelector("#account-orders");
+
+addAllEvents();
+
+// addEventListener들을 묶어주는 코드
+function addAllEvents() {
+  accountSecurity.addEventListener("click", onClickAccountSecurity);
+  accountOrders.addEventListener("click", onClickAccountOrders);
+}
+
+function onClickAccountSecurity() {
+  window.location.href = "../account-security/account-security.html";
+}
+
+function onClickAccountOrders() {
+  window.location.href = "../account-orders/account-orders.html";
+}
